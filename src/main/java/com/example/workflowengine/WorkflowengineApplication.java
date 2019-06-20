e8b39d17cc79a7716eff6238cba4370add7cc084
wangxiaoke <xiaoke.wang@ds-mc.cn>
2019-06-20 16:07:29 +0800

    初始化项目

---
+++ src/main/java/com/example/workflowengine/WorkflowengineApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflowengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowengineApplication.class, args);
+	}
+
+}
